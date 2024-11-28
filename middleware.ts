03ae1b90230ee7d885f840869b85fed2b2a4c58c
Mitesh Chaudhari <109075376+meet931@users.noreply.github.com>
2024-11-28 11:47:08 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  const adminData = request.cookies.get("adminData")?.value;
+  console.log(adminData)
+
+  if(!adminData){    
+      return NextResponse.redirect(new URL('/auth/login', request.url))    
+  }
+
+  return NextResponse.next();
 }
+
  
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/dashboard/:path*',
 }
