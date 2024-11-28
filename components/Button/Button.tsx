cba7de15ce76f0a336d88a79cb9a8d47a7d8b181
Mitesh Chaudhari <109075376+meet931@users.noreply.github.com>
2024-11-28 11:44:30 +0530

    Update Button.tsx

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,22 +1,30 @@
-import React, { FC } from "react";
+import React, { ButtonHTMLAttributes, FC } from "react";
 
-interface ButtonProps {
-    buttonName: string;
-    onClick: () => void;
-    className: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonName: string;
+  onClick?: () => void;
+  className?: string;  
 }
 
-const Button: FC<ButtonProps> = ({ buttonName, onClick, className }) => {
+const Button: FC<ButtonProps> = ({
+  buttonName,
+  onClick,
+  className,
+  type = "button",
+  ...props
+}) => {
   return (
     <div>
       <button
+      type={type}
         className={`px-4 py-2 rounded-md ${className}`}
         onClick={onClick}
+        {...props}
       >
         {buttonName}
       </button>
     </div>
   );
-}
+};
 
 export default Button;
